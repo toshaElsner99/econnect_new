3c7421b492515b4d2c28375e437cebc157e50938
jigar <jigargodasara@elsner.com>
2025-03-10 17:12:37 +0530

    -- Search functionallity

---
+++ lib/model/searchMessages.dart
@@ -0,0 +1,179 @@
+// To parse this JSON data, do
+//
+//     final searchMessage = searchMessageFromJson(jsonString);
+
+import 'dart:convert';
+
+List<SearchMessage> searchMessageFromJson(String str) => List<SearchMessage>.from(json.decode(str).map((x) => SearchMessage.fromJson(x)));
+
+String searchMessageToJson(List<SearchMessage> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class SearchMessage {
+  DateTime id;
+  List<Message> messages;
+  int totalMessages;
+  DateTime date;
+
+  SearchMessage({
+    required this.id,
+    required this.messages,
+    required this.totalMessages,
+    required this.date,
+  });
+
+  factory SearchMessage.fromJson(Map<String, dynamic> json) => SearchMessage(
+    id: DateTime.parse(json["_id"]),
+    messages: List<Message>.from(json["messages"].map((x) => Message.fromJson(x))),
+    totalMessages: json["totalMessages"],
+    date: DateTime.parse(json["date"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "_id": "${id.year.toString().padLeft(4, '0')}-${id.month.toString().padLeft(2, '0')}-${id.day.toString().padLeft(2, '0')}",
+    "messages": List<dynamic>.from(messages.map((x) => x.toJson())),
+    "totalMessages": totalMessages,
+    "date": "${date.year.toString().padLeft(4, '0')}-${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')}",
+  };
+}
+
+class Message {
+  String id;
+  String content;
+  DateTime createdAt;
+  dynamic replyTo;
+  bool isForwarded;
+  String senderId;
+  ErInfo senderInfo;
+  ChannelInfo? channelInfo;
+  String? channelId;
+  ErInfo oppositeUserInfo;
+  String? receiverId;
+
+  Message({
+    required this.id,
+    required this.content,
+    required this.createdAt,
+    required this.replyTo,
+    required this.isForwarded,
+    required this.senderId,
+    required this.senderInfo,
+    this.channelInfo,
+    this.channelId,
+    required this.oppositeUserInfo,
+    this.receiverId,
+  });
+
+  factory Message.fromJson(Map<String, dynamic> json) => Message(
+    id: json["_id"],
+    content: json["content"],
+    createdAt: DateTime.parse(json["createdAt"]),
+    replyTo: json["replyTo"],
+    isForwarded: json["isForwarded"],
+    senderId: json["senderId"],
+    senderInfo: ErInfo.fromJson(json["senderInfo"]),
+    channelInfo: json["channelInfo"] == null ? null : ChannelInfo.fromJson(json["channelInfo"]),
+    channelId: json["channelId"],
+    oppositeUserInfo: ErInfo.fromJson(json["oppositeUserInfo"]),
+    receiverId: json["receiverId"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "_id": id,
+    "content": content,
+    "createdAt": createdAt.toIso8601String(),
+    "replyTo": replyTo,
+    "isForwarded": isForwarded,
+    "senderId": senderId,
+    "senderInfo": senderInfo.toJson(),
+    "channelInfo": channelInfo?.toJson(),
+    "channelId": channelId,
+    "oppositeUserInfo": oppositeUserInfo.toJson(),
+    "receiverId": receiverId,
+  };
+}
+
+class ChannelInfo {
+  String id;
+  String name;
+  String ownerId;
+  bool isDefault;
+
+  ChannelInfo({
+    required this.id,
+    required this.name,
+    required this.ownerId,
+    required this.isDefault,
+  });
+
+  factory ChannelInfo.fromJson(Map<String, dynamic> json) => ChannelInfo(
+    id: json["_id"],
+    name: json["name"],
+    ownerId: json["ownerId"],
+    isDefault: json["isDefault"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "_id": id,
+    "name": name,
+    "ownerId": ownerId,
+    "isDefault": isDefault,
+  };
+}
+
+class ErInfo {
+  String id;
+  String username;
+  String email;
+  Status status;
+  String thumbnailAvatarUrl;
+  String elsnerEmail;
+
+  ErInfo({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.status,
+    required this.thumbnailAvatarUrl,
+    required this.elsnerEmail,
+  });
+
+  factory ErInfo.fromJson(Map<String, dynamic> json) => ErInfo(
+    id: json["_id"],
+    username: json["username"],
+    email: json["email"],
+    status: statusValues.map[json["status"]]!,
+    thumbnailAvatarUrl: json["thumbnail_avatarUrl"],
+    elsnerEmail: json["elsner_email"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "_id": id,
+    "username": username,
+    "email": email,
+    "status": statusValues.reverse[status],
+    "thumbnail_avatarUrl": thumbnailAvatarUrl,
+    "elsner_email": elsnerEmail,
+  };
+}
+
+enum Status {
+  OFFLINE,
+  ONLINE
+}
+
+final statusValues = EnumValues({
+  "offline": Status.OFFLINE,
+  "online": Status.ONLINE
+});
+
+class EnumValues<T> {
+  Map<String, T> map;
+  late Map<T, String> reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String> get reverse {
+    reverseMap = map.map((k, v) => MapEntry(v, k));
+    return reverseMap;
+  }
+}
---
+++ lib/screens/find_message_screen/find_message_screen.dart
@@ -0,0 +1,225 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:e_connect/model/searchMessages.dart';
+import 'package:e_connect/utils/api_service/api_string_constants.dart';
+import 'package:e_connect/utils/app_color_constants.dart';
+import 'package:e_connect/utils/app_string_constants.dart';
+import 'package:e_connect/utils/common/common_function.dart';
+import 'package:e_connect/utils/common/common_widgets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+
+import '../../main.dart';
+import '../../providers/common_provider.dart';
+import '../../providers/search_message_provider.dart';
+import '../../utils/app_image_assets.dart';
+import '../../utils/app_preference_constants.dart';
+
+class FindMessageScreen extends StatefulWidget {
+  const FindMessageScreen({super.key});
+
+  @override
+  State<FindMessageScreen> createState() => _FindMessageScreenState();
+}
+
+class _FindMessageScreenState extends State<FindMessageScreen> {
+  final _searchController = TextEditingController();
+  final ScrollController scrollController = ScrollController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          titleSpacing: 0,
+          leading: IconButton(
+            icon: const Icon(Icons.close,color: Colors.white,),
+            onPressed: () => Navigator.pop(context,),
+          ),
+          title: commonText(
+            text: 'Find Messages',
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Search Field
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: commonTextFormField(
+                controller: _searchController,
+                hintText: 'Search Messages',
+                prefixIcon: const Icon(CupertinoIcons.search),
+                fieldSubmitted: (value) {
+                  context.read<SearchMessageProvider>().browseAndSearchMessages(search: value!);
+                },
+              ),
+            ),
+            Expanded(
+              child: Consumer<SearchMessageProvider>(
+                builder: (context,value,a){
+                  print("value.messageGroups.length");
+                  print(value.messageGroups.length);
+                  if(value.messageGroups.length == 0){
+                    return Center(child: commonText(text: "No data found"),);
+                  }
+                  return ListView.builder(
+                    itemCount: value.messageGroups.length,
+                    itemBuilder: (context, index) {
+                      final messageData = value.messageGroups[index];
+                      final dateFormatted =formatDateTime(DateTime.parse(messageData.date.toString()));
+
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 15.0,top: 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Row(
+                                children: [
+                                  Expanded(child: Divider()), // Left side line
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                                    child: commonText(
+                                      text: dateFormatted,
+                                        fontSize: 13,color: Colors.grey
+                                    ),
+                                  ),
+                                  Expanded(child: Divider()), // Right side line
+                                ],
+                              ),
+                            ),
+                            ListView.builder(
+                              shrinkWrap: true,
+                              physics: NeverScrollableScrollPhysics(),
+                              itemCount: messageData.messages.length,
+                              itemBuilder: (context, msgIndex) {
+                                final message = messageData.messages[msgIndex];
+                                final sender = message.senderInfo.username;
+                                final channel = message.channelInfo?.name ?? '';
+                                final content = message.content;
+                                final isForwarded = message.isForwarded;
+
+                                return Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                                  child: InkWell(
+                                    onTap: ()async{
+                                      // print("Is From Channel ${message.channelInfo != null ? true : false}");
+                                      await value.getPageNumber(messageId: message.id, senderId: message.senderId,receiverId: message.receiverId!).then((int pageNumber) {
+                                        print("Page number $pageNumber");
+                                        Navigator.pop(context,{"id":message.senderInfo.id,"oppositeUserID":message.oppositeUserInfo.id,"oppositeUserName":message.oppositeUserInfo.username,"name":message.senderInfo.username,"needToOpenChannelChat":message.channelInfo != null ? true : false,"channelId":message.channelInfo != null ? message.channelInfo?.id : "","pageNO":pageNumber});
+                                      });
+                                    },
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        SizedBox(height: 8,),
+                                        commonText(text : channel != "" ? channel :"Direct Message (With ${message.oppositeUserInfo.username})",fontSize: 18,color: Colors.grey),
+                                        SizedBox(height: 10,),
+                                        Row(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Stack(
+                                              alignment: Alignment.bottomRight,
+                                              children: [
+                                                CircleAvatar(
+                                                  radius: 25,
+                                                  backgroundColor: Colors.grey[200],
+                                                  child: ClipOval(
+                                                    child: CachedNetworkImage(
+                                                      imageUrl: signInModel.data?.user?.id == message.senderInfo.id
+                                                          ? ApiString.profileBaseUrl + (signInModel.data!.user!.thumbnailAvatarUrl ?? '')
+                                                          : ApiString.profileBaseUrl + (message.senderInfo.thumbnailAvatarUrl ?? ''),
+                                                      width: 25 * 2,
+                                                      height: 25 * 2,
+                                                      fit: BoxFit.cover,
+                                                      placeholder: (context, url) => CircularProgressIndicator(
+                                                        strokeWidth: 2,
+                                                      ),
+                                                      errorWidget: (context, url, error) => Icon(Icons.error, size: 25),
+                                                    ),
+                                                  ),
+                                                ),
+                                                  Stack(
+                                                    alignment: Alignment.center,
+                                                    children: [
+                                                      Container(
+                                                        height: 20,
+                                                        width: 20,
+                                                        decoration: BoxDecoration(
+                                                          color: message.senderInfo.status.name.contains("offline") ? Colors.transparent : Colors.white,
+                                                          shape: BoxShape.circle,
+                                                        ),
+                                                      ),
+                                                      getCommonStatusIcons(status: message.senderInfo.status.name, size: 20, assetIcon: false),
+                                                    ],
+                                                  ),
+                                              ],
+                                            ),
+                                            // profileIconWithStatus(
+                                            //   userID: message.senderInfo.id,
+                                            //   status: "",
+                                            //   needToShowIcon: false,
+                                            //   radius: 12,
+                                            //   otherUserProfile: message.senderInfo.thumbnailAvatarUrl,
+                                            //   containerSize: 20
+                                            // ),
+                                            SizedBox(width: 10),
+                                            Expanded(
+                                              child: Column(
+                                                crossAxisAlignment: CrossAxisAlignment.start,
+                                                children: [
+                                                  commonText(text : sender,fontSize: 18),
+                                                  SizedBox(height: 10),
+                                                  commonHTMLText(message: content),
+                                                ],
+                                              ),
+                                            ),
+                                            // if (isForwarded) Icon(Icons.forward),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                );
+                                // return ListTile(
+                                //   leading: CircleAvatar(
+                                //     backgroundImage: NetworkImage(message.senderInfo.thumbnailAvatarUrl),
+                                //   ),
+                                //   title: Text(sender),
+                                //   subtitle: Column(
+                                //     crossAxisAlignment: CrossAxisAlignment.start,
+                                //     children: [
+                                //       channel != "" ? Text('Channel: $channel') : Text("Direct Message ( With ${message.oppositeUserInfo.username})"),
+                                //       SizedBox(height: 4),
+                                //       Text(content),
+                                //     ],
+                                //   ),
+                                //   trailing: isForwarded ? Icon(Icons.forward) : null,
+                                // );
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+}
